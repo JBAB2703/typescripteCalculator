5a8fc7df778db0e44c470224fd7d12d9c274b805
jkylecarpentry@gmail.com <jkylecarpentry@gmail.com>
2019-09-10 18:47:15 -0400

    ts calculator

---
+++ js/Calculator.js
@@ -0,0 +1,17 @@
+"use strict";
+var a = 1;
+var b = 4;
+var c = 5;
+var d = 2;
+function add(a, b) {
+    return a + b;
+    return b - a;
+    return a * b;
+    return b / d;
+    return c % d;
+}
+console.log(a + b);
+console.log(b - a);
+console.log(a * b);
+console.log(b / d);
+console.log(c % d);
---
+++ ts/Calculator.ts
@@ -0,0 +1,18 @@
+let a: number = 1;
+let b: number = 4;
+let c: number = 5;
+let d: number = 2;
+
+
+function add(a:number, b:number):number {
+    return a + b;
+    return b - a;
+    return a * b;
+    return b / d;
+    return c % d;
+}
+console.log(a+b);
+console.log(b-a);
+console.log(a*b);
+console.log(b/d);
+console.log(c%d);
